c7d6dae3bcd3dd7f9abdec9cb8d54bb07f5dc25e
Asep Gunawan <asgun020@gmail.com>
2022-03-25 16:36:27 +0700

    Tugas 2

---
+++ 5-Yargs-2/app.js
@@ -0,0 +1,154 @@
+// const fs = require('fs');
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// const dirPath = './data';
+// if (!fs.existsSync('dirPath')) {
+//     fs.mkdirSync(dirPath);
+// }
+// const dataPath = './data/contacts.json';
+// if (!fs.existsSync(dataPath)) {
+//     fs.writeFileSync(dataPath, '[]', 'utf-8')
+// }
+
+
+// const questions = (ask) => {
+//     return new Promise((resolve, reject) => rl.question(ask, (inputVariable) => {
+//         resolve(inputVariable)
+//     }))
+// }
+
+// const main = async () => {
+//     const name = await questions('What is your name: ');
+//     const mobile = await questions('Your phone number: ');
+//     const email = await questions('What is your email: ')
+
+//     const contact = { name, mobile, email };
+
+//     const file = fs.readFileSync('data/contacts.json', 'utf8');
+//     const contacts = JSON.parse(file);
+//     contacts.push(contact);
+//     fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
+//     console.log('Data is saved');
+//     rl.close();
+// }
+
+// main()
+
+/* rl.question('What is your name: ', (name) => {
+    rl.question('What is your phone number: ', (mobile) => {
+        const contact = { name, mobile };
+
+        const dirPath = './data';
+        if (!fs.existsSync('dirPath')) {
+            fs.mkdirSync(dirPath);
+        }
+        const dataPath = './data/contacts.json';
+        if (!fs.existsSync(dataPath)) {
+            fs.writeFileSync(dataPath, '[]', 'utf-8')
+        }
+
+        const file = fs.readFileSync('data/contacts.json', 'utf8');
+        const contacts = JSON.parse(file);
+        contacts.push(contact);
+        fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
+        console.log('Data is saved');
+        rl.close();
+    });
+}); */
+
+// const contacts = require('./contacts.js');
+
+// const main = async () => {
+//     const name = await contacts.questions('What is your name: ');
+//     const email = await contacts.questions('What is your email: ');
+//     const mobile = await contacts.questions('What is your mobile: ');
+
+//     contacts.saveContact(name, email, mobile);
+// };
+// main()
+
+const yargs = require('yargs');
+const contacts = require('./contacts.js'); //call contacts.js
+
+yargs.command({
+    command: 'add',
+    describe: 'add new contact',
+    builder: {
+        name: {
+            describe: 'Contact Name',
+            demandOption: true,
+            type: 'string',
+        },
+        email: {
+            describe: 'Contact Email',
+            demandOption: false,
+            type: 'string',
+        },
+        mobile: {
+            describe: 'Contact Mobile Phone Number',
+            demandOption: true,
+            type: 'string',
+        },
+    },
+
+    handler(argv) {
+        const contact = {
+            name: argv.name,
+            email: argv.email,
+            mobile: argv.mobile,
+        };
+        console.log(contact);
+        //save input to json file
+        contacts.saveContact(argv.name, argv.email, argv.mobile);
+    },
+});
+
+// command to see list of information from json file
+yargs.command({
+    command: 'list',
+    describe: 'see contact list',
+
+    handler() {
+        contacts.listContact();
+    },
+});
+
+// commmand to see detail information from contact.json file
+yargs.command({
+    command: 'detail',
+    describe: 'detail information',
+    builder: {
+        name: {
+            describe: 'Contact Name',
+            demandOption: true,
+            type: 'string',
+        },
+    },
+
+    handler(argv) {
+        contacts.detailContact(argv.name);
+    },
+});
+
+// command to delete data from contacts.json file
+yargs.command({
+    command: 'delete',
+    describe: 'delete data',
+    builder: {
+        name: {
+            describe: 'Contact Name',
+            demandOption: true,
+            type: 'string',
+        },
+    },
+
+    handler(argv) {
+        contacts.deleteContact(argv.name);
+    },
+});
+
+yargs.parse();
---
+++ 5-Yargs-2/contacts.js
@@ -0,0 +1,100 @@
+const fs = require('fs');
+const validator = require('validator');
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+const dirPath = './data';
+if (!fs.existsSync('dirPath')) {
+    fs.mkdirSync(dirPath);
+}
+const dataPath = './data/contacts.json';
+if (!fs.existsSync(dataPath)) {
+    fs.writeFileSync(dataPath, '[]', 'utf-8')
+}
+
+// const questions = (ask) => {
+//     return new Promise((resolve, reject) => rl.question(ask, (inputVariable) => {
+//         resolve(inputVariable)
+//     }))
+// }
+const saveContact = (name, email, mobile) => {
+
+    const contact = { name, mobile, email };
+
+    // to read file 
+    // const file = fs.readFileSync('data/contacts.json', 'utf8');
+    // const contacts = JSON.parse(file);
+
+    const contacts = loadContact();
+    // to check if the data has been recorded before
+    const duplicateName = contacts.find((contact) => contact.name === name);
+
+    //check if there is duplicate name
+    if (duplicateName) {
+        console.log('Contact name is already recorded, Use another contact name.');
+        return false;
+    }
+
+    // validate email format, also check if there is duplicate email 
+    if (email) {
+        if (!validator.isEmail(email)) {
+            console.log('Invalid email.');
+            return false;
+        }
+    }
+
+    // validate mobile phone format, also check if there is duplicate mobile phone 
+    if (!validator.isMobilePhone(mobile)) {
+        console.log('Invalid phone number.');
+        return false;
+    }
+
+    // save data to file
+    contacts.push(contact);
+    fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
+    console.log('Data is saved');
+
+}
+
+const loadContact = () => {
+    const file = fs.readFileSync('data/contacts.json', 'utf8');
+    const contacts = JSON.parse(file);
+    return contacts;
+}
+
+const listContact = () => {
+    const contacts = loadContact();
+    console.log('Contact List: ');
+    contacts.forEach((contact, i) => {
+        console.log(`${i + 1}. ${contact.name} - ${contact.mobile}`);
+    });
+};
+
+const detailContact = (name) => {
+    const contacts = loadContact();
+    contactName = { name }
+    const detailInformation = contacts.find((contactName) => contactName.name === name)
+    if (detailInformation) {
+        console.log(`${detailInformation.name}\n${detailInformation.email}\n${detailInformation.mobile}`)
+    } else {
+        console.log('Name is not found')
+    }
+}
+
+const deleteContact = (name) => {
+    const contacts = loadContact()
+    contactName = { name }
+    const deleteInformation = contacts.filter((contactName) => contactName.name !== name)
+    if (contacts.length === deleteInformation.length) {
+        console.log('Data is not found');
+        return false
+    }
+    fs.writeFileSync('data/contacts.json', JSON.stringify(deleteInformation));
+    console.log('Data has been deleted')
+}
+
+module.exports = { saveContact, listContact, detailContact, deleteContact }
+// module.exports.saveContact = saveContact
